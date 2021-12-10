4a0fddfdfcdf1e3d6a01cc0e93e07b88431e8a81
taranehkhosrojerdi <87570595+taranehkhosrojerdi@users.noreply.github.com>
2021-12-10 09:47:17 -0800

    add quesion5

---
+++ question5/javascript/permission.js
@@ -0,0 +1,12 @@
+<script LANGUAGE="JavaScript">
+function confirmSubmit()
+{
+var agree=confirm("Are you sure you wish to send message?");
+if (agree){
+ return true ;
+	
+}
+else
+ return false ;
+}
+</script>
